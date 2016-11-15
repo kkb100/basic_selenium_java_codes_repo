2fd5262577f9b4fbc1111915d7c6ab45d2110066
Kelvin Boakye <kkboakye45@yahoo.co.uk>
2016-11-15 07:26:06 +0000

    Updated test

---
+++ FirstSelenium_Maven/Magento_maven_project1/src/main/java/co_uk_magento/pages/EcomSetup.java
@@ -0,0 +1,21 @@
+package co_uk_magento.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+	public class EcomSetup {
+	
+
+		public WebDriver browserSetup() {
+			
+		String geckoPath = "C:\\Automation\\projects\\github_opensource_repos\\basic_selenium_java_codes_repo\\FirstSelenium\\drivers\\geckodriver-v0.11.1-win64\\geckodriver.exe";
+		System.setProperty("webdriver.gecko.driver", geckoPath);
+		WebDriver driver = new FirefoxDriver();
+			return driver;	
+		}
+	
+	
+	
+	
+
+}
---
+++ FirstSelenium_Maven/Magento_maven_project1/src/test/java/co_uk_magento_testRunner/EcomRunPage.java
@@ -0,0 +1,88 @@
+package co_uk_magento_testRunner;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import co_uk_magento.pages.EcomLoginPage;
+import co_uk_magento.pages.EcomSetup;
+import co_uk_magento.pages.EcomValidate;
+
+
+
+public class EcomRunPage {
+	
+	
+	 @BeforeClass
+	   public static void connectToDd(){
+		   System.out.println("Connection Establish");
+		   
+	   }
+	 
+	 
+	 @AfterClass
+	   public static void quitDd() {
+		   System.out.println("Connection closed");   
+	   }
+	
+
+	private static WebDriver driver;
+	private EcomLoginPage ecomloginpage;
+	private EcomValidate ecomvalidate;
+	
+		@Before 
+		public void setUpTest(){
+			EcomSetup ecomsetup = new EcomSetup();	
+			WebDriver driver = ecomsetup.browserSetup();
+			ecomloginpage = new EcomLoginPage(driver);
+			ecomloginpage.loadingBrowser();
+			ecomvalidate = new EcomValidate(driver);
+			
+		}
+		
+		@After
+		public void endUpTest(){
+			ecomloginpage.tearDown();
+           }
+		
+		
+		@Test
+        public void logTestLogin() {
+        	ecomloginpage.enterUrl("http://live.guru99.com");
+        	ecomloginpage.loginIn("test2@kkb.com", "123456");
+        	ecomvalidate.validatetest("John testing2!");
+        	
+         	}
+	
+
+        @Test 
+        public void testLogOut() {
+        	ecomloginpage.enterUrl("http://live.guru99.com");
+        	ecomloginpage.loginIn("test@kkb.com", "123456");
+        	ecomvalidate.validatetest("John testing!");
+        	ecomloginpage.logOut();
+        	
+        	
+         	}
+        
+        @Test 
+        public void logTestUser2() {
+        	ecomloginpage.enterUrl("http://live.guru99.com");
+        	ecomloginpage.loginIn("test@kkb.com", "123456");
+        	
+         	}
+       
+    
+        @Test 
+        public void testInvalidLog() {
+        ecomloginpage.enterUrl("http://live.guru99.com");
+        ecomloginpage.invalidPasswordLogin("test2@kkb.com", "232234");
+        ecomvalidate.validatetest("Invalid login or password.");
+        	
+        }
+ 
+}
+
