0f06501d0cac0277ebd5619a454c113566148560
Kelvin Boakye <Kelvin.Boakye@river-island.com>
2016-11-29 17:19:36 +0000

    Added test for getAttributes

---
+++ FirstSelenium/src/Module11/Gmail.java
@@ -0,0 +1,40 @@
+package Module11;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Gmail {
+
+
+	public static void main(String[] args) {
+	
+	String Broswer = "Chrome";
+	
+	String chromepath = System.getProperty("user.dir") + "\\drivers\\chromedriver_win32\\chromedriver.exe";
+    System.setProperty("webdriver.chrome.driver", chromepath);
+    
+    WebDriver driver = null;
+    
+    if(Broswer==("Chrome")) {
+    driver = new ChromeDriver();
+    }
+    
+    driver.manage().window().maximize();
+    driver.get("http://gmail.com");
+    String text= driver.findElement(By.id("next")).getAttribute("value");
+    
+   String text1 = driver.findElement(By.xpath("html/body/div/div[2]/div[1]/h1")).getText();
+   
+   System.out.println(text);
+   System.out.println(text1);
+   
+   driver.close();
+		
+
+	}
+
+}
--- FirstSelenium/src/Module11/LaunchBrowsers.java
+++ FirstSelenium/src/Module11/LaunchBrowsers.java
@@ -1,7 +1,9 @@
 package Module11;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -10,24 +12,39 @@ public class LaunchBrowsers {
 	String Browser = "Chrome";
 	
 	@Test
-	public void launchTest() {
+	public void launchTest() throws InterruptedException {
 		
 	//String FFPath = "C:\\Automation\\projects\\github_opensource_repos\\basic_selenium_java_codes_repo\\FirstSelenium\\drivers\\geckodriver-v0.11.1-win64\\geckodriver.exe";
 	//System.setProperty("webdriver.gecko.driver", FFPath);
+		
+	String ans = System.getProperty("user.dir");
 	
-	String ChromPath = "C:\\Automation\\projects\\github_opensource_repos\\basic_selenium_java_codes_repo\\FirstSelenium\\drivers\\chromedriver_win32\\chromedriver.exe";
+	System.out.println(ans);
+	
+	
+	String ChromPath = System.getProperty("user.dir") + "\\drivers\\chromedriver_win32\\chromedriver.exe";
 	System.setProperty("webdriver.chrome.driver", ChromPath);
 	
 	WebDriver driver = null;
 	
-	if (Browser.equals("Firefox")) {
+	if (Browser==("Firefox")) {
 		driver = new FirefoxDriver();
-	}else if (Browser.equals("Chrome")) {
+	}else if (Browser==("Chrome")) {
 		driver = new ChromeDriver();
 	}
 	
-	driver.get("http://facebook.com");
+	driver.get("https://www.google.co.uk/");
+	WebElement search_feild = driver.findElement(By.id("lst-ib"));
+	search_feild.sendKeys("Boy games");
+	Thread.sleep(3000);
+	
+	WebElement result_list = driver.findElement(By.xpath("//*[@id='sbse1']/div[2]/b"));
+	result_list.click();
+	
 	driver.quit();
 	}
+	
+	
+	
 
 }
